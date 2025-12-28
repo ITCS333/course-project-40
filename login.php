717572a0994fe07a18aff459cc878e6ed213a504
AliJalal05 <202309996@stu.uob.edu.bh>
2025-12-28 18:38:47 +0300

    Update login.php

--- login.php
+++ login.php
@@ -3,41 +3,47 @@
 session_start();
 
 // Temporary list of users:
-$users=[
-  'admin@example.com' => [
-  'password' => 'admin1587', 
-  'role' => 'admin',
-  ],
-  ];
-
+$users = [
+    'admin@example.com' => [
+        'password' => password_hash('admin1587', PASSWORD_DEFAULT),
+        'role' => 'admin',
+    ],
+];
 
 //Check if form is submitted by POST
-if($_SERVER["REQUEST_METHOD"]==="POST") {
-  $email= $_POST['email'] ?? '';
-  $password= $_POST['password'] ?? '';
-  // To clean extra spaces:
-  $email =trim($email);
-  $password= trim($password);
-  // Validating email and pass
-  if(isset($users[$email]) && $users[$email] ['password'] === $password) {
-    // if successful login, then store the data of the user in the session
-    $_SESSION["email"] =$email;
-    $_SESSION["role"] =$users[$email]['role'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-       //Redirect based on role of user
-       if ($_SESSION['role'] ==='admin') {
-         header('Location: admin_portal.php');
-         exit;
-       }
-       else{ header('Location: resources.php'); exit; }
-  }
-  else{ // If Invalid login, then redirect back to login page with error 
-       header('Location: login.html?error=1');
-       exit; }
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    // To clean extra spaces:
+    $email = trim($email);
+    $password = trim($password);
+
+    // Validating email and pass
+    if (
+        isset($users[$email]) &&
+        password_verify($password, $users[$email]['password'])
+    ) {
+        // if successful login, then store the data of the user in the session
+        $_SESSION["email"] = $email;
+        $_SESSION["role"] = $users[$email]['role'];
+
+        //Redirect based on role of user
+        if ($_SESSION['role'] === 'admin') {
+            header('Location: admin_portal.php');
+            exit;
+        } else {
+            header('Location: resources.php');
+            exit;
+        }
+    } else {
+        // If Invalid login, then redirect back to login page with error 
+        header('Location: login.html?error=1');
+        exit;
+    }
+} else {
+    //If a user opened login.php without POST, then redirect to login page
+    header('location: login.html');
+    exit;
 }
-else { 
-  //If a user opened login.php without POST, then redirect to login page
-  header('location: login.html');
-  exit; }
-    
-    
